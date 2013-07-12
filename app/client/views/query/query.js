9fb748723c27a1f1a64bceac1bb2552b80fe1e13
Juan Sepulveda <juanjsepulveda@gmail.com>
2013-07-11 19:44:10 -0700

    Fixtures keywords

--- app/client/views/query/query.js
+++ app/client/views/query/query.js
@@ -1,11 +1,26 @@
+
 Template.query.queryResults = function  () {
 	return Session.get('queryResults')
+}
+
+Template.query.isSearching = function  () {
+	return Session.get("querySearching");
+}
+Template.query.isDataLoaded = function  () {
+	return Session.get("queryDataLoaded");
 }
 
 Template.query.events({
 	"keyup #input":function  () {
 		value = $("#input").val();
-		data = Keywords.find({name:{$regex:"/*"+value+"*"}});
+		Session.set("querySearching",true);
+		Session.set("queryDataLoaded",null);
+
+		data = Keywords.find({name:{$regex:"/*"+value+"*"}},{limit:10});
+
+		console.log(data.db_objects); 
+		Session.set("querySearching",null);
+		Session.set("queryDataLoaded",true);
 		Session.set("queryResults",data.db_objects)	
 	}
 });

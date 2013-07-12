01e9d941f7f507108d83566c3d945ebff0ee511b
Juan Sepulveda <juanjsepulveda@gmail.com>
2013-07-11 21:16:07 -0700

    Add initial searching logic

--- app/client/views/query/query.js
+++ app/client/views/query/query.js
@@ -9,18 +9,33 @@ Template.query.isSearching = function  () {
 Template.query.isDataLoaded = function  () {
 	return Session.get("queryDataLoaded");
 }
-
 Template.query.events({
 	"keyup #input":function  () {
 		value = $("#input").val();
+
 		Session.set("querySearching",true);
 		Session.set("queryDataLoaded",null);
+		datos = [];
 
-		data = Keywords.find({name:{$regex:"/*"+value+"*"}},{limit:10});
+		if(!value)
+		{
+		Session.set("querySearching",false);
+			return 
+		}
+		Meteor.call("autocomplete",value,function  (error,result) {
+			data = Keywords.find({name:{$regex:"/*"+value+"*"}},{limit:10});
+			data.forEach(function  (doc) {
+				datos.push(doc);
+			})
+			// console.log(data.collection.docs);
+			// console.log(datos);
+			Session.set("querySearching",null);
+			Session.set("queryDataLoaded",true);
+			Session.set("queryResults",datos);	
+			
+		});
 
-		console.log(data.db_objects); 
-		Session.set("querySearching",null);
-		Session.set("queryDataLoaded",true);
-		Session.set("queryResults",data.db_objects)	
+
+		// console.log(data.db_objects); 
 	}
 });
--- app/collections/keywords.js
+++ app/collections/keywords.js
@@ -1,1 +1,12 @@
 Keywords = new Meteor.Collection("keywords");
+
+Meteor.methods({
+	"autocomplete":function  (query) {
+		// console.log(query)
+		subscription = Meteor.subscribe("autocompleteKeywords",query,function  () {
+			Keywords.find().fetch()
+			subscription.stop();
+		});
+		// Keywords.find();
+	}
+});

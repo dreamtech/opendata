cb7e359af448aa50b6632fbdef19f770b6104061
Ranfis S <ranfis.sh@gmail.com>
2013-07-12 21:57:06 -0400

    Tamo broken

--- app/client/views/home/search.js
+++ app/client/views/home/search.js
@@ -1,35 +1,3 @@
-Template.search.queryResults = function  () {
-	return Session.get('queryResults')
-}
-
-Template.search.isSearching = function  () {
-	return Session.get("querySearching");
-}
-Template.search.isDataLoaded = function  () {
-	return Session.get("queryDataLoaded");
-}
-Template.search.events({
-	"keyup #input":function  () {
-		value = $("#input").val();
-
-		Session.set("querySearching",true);
-		Session.set("queryDataLoaded",null);
-
-		if(!value)
-		{
-			Session.set("querySearching",false);
-			return 
-		}
-		Meteor.call("autocomplete",value,function  (error,result) {
-			data = Keywords.find().fetch()
-			
-			Session.set("querySearching",null);
-			Session.set("queryDataLoaded",true);
-			Session.set("queryResults",data);	
-			
-		});
-	}
-});
 
 Template.search.queryResults = function  () {
 	return Session.get('queryResults')
@@ -135,4 +103,5 @@ Deps.autorun(function() {
 	}catch(ex){
 		console.log("Explotion!!");
 	}
+
 });
